5eab762f5b313c98975db74e2f8c38f5dfcf9376
J-CHOO <cnwns820@naver.com>
2021-12-31 00:39:36 +0900

    키패드 누르기 수정완료

--- Programmers/level1/키패드 누르기.py
+++ Programmers/level1/키패드 누르기.py
@@ -1,8 +1,9 @@
 def solution(numbers, hand):
+    answer = ""
     key_pad = {
-        1:[0,0], 2:[1,0], 3:[2,0]
-        4:[0,1], 5:[1,1], 6:[2,1]
-        7:[0,2], 8:[1,2], 9:[2,2]
+        1:[0,0], 2:[1,0], 3:[2,0],
+        4:[0,1], 5:[1,1], 6:[2,1],
+        7:[0,2], 8:[1,2], 9:[2,2],
         "*":[0,3], 0:[1,3], "#":[2,3]}
     L_hand = key_pad["*"]
     R_hand = key_pad["#"]
@@ -14,8 +15,22 @@ def solution(numbers, hand):
             answer += "R"
             R_hand = key_pad[num]
         else:
-            pass
-
+            L_dis = abs(L_hand[0]-key_pad[num][0])+abs(L_hand[1]-key_pad[num][1])
+            R_dis = abs(R_hand[0]-key_pad[num][0])+abs(R_hand[1]-key_pad[num][1])
             
-    print(answer)
+            if L_dis < R_dis:
+                answer += "L"
+                L_hand = key_pad[num]
+            elif L_dis > R_dis:
+                answer += "R"
+                R_hand = key_pad[num]
+            else:
+                if hand == "left":
+                    answer += "L"
+                    L_hand = key_pad[num]
+                else:
+                    answer += "R"
+                    R_hand = key_pad[num]
+            
+    
     return answer
