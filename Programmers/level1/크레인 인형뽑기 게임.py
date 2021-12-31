7d0d341a5348ccaee16dafaf9c2a97158303bc45
J-CHOO <cnwns820@naver.com>
2021-12-31 20:09:51 +0900

    크레인 인형뽑기 게임

---
+++ Programmers/level1/크레인 인형뽑기 게임.py
@@ -0,0 +1,24 @@
+def solution(board, moves):
+    answer = 0
+    basket = []
+    
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] == 0:
+                pass
+            else:
+                if len(basket) == 0:
+                    basket.append(board[j][i-1])
+                    board[j][i-1] = 0
+                else:
+                    if basket[-1] == board[j][i-1]:
+                        basket.pop()
+                        board[j][i-1] = 0
+                        answer += 2
+                    else:
+                        basket.append(board[j][i-1])
+                        board[j][i-1] = 0
+                break                 
+            
+    return answer
+#break를 안걸어줘서 board[j][i-1]에 있는 원소를 전부 끝까지 basket에 추가했어서 계속 답이 안나옴
