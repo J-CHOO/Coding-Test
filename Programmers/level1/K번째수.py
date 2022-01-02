44a5e24727afd3d78630089756d1bc8378396f6f
J-CHOO <cnwns820@naver.com>
2022-01-03 08:42:25 +0900

    K번째수 완료!

---
+++ Programmers/level1/K번째수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+    temp = []
+    for i in commands:
+        temp = array[i[0]-1:i[1]]
+        temp.sort()
+        answer.append(temp[i[2]-1])
+        temp = []
+        
+            
+    return answer
